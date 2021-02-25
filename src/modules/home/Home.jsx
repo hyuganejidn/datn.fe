fabf60925743b823813e39ded1466e16047e3ff2
Hung.VX <hyuganejidn@gmail.com>
2021-02-25 14:30:02 +0700

    no cm

--- src/modules/home/Home.jsx
+++ src/modules/home/Home.jsx
@@ -4,6 +4,8 @@ import { Auth } from '../auth/Auth'
 
 function Home() {
   const dispatch = useDispatch()
+  console.log('hung test  123')
+
   return (
     <div>
       <button type="button" onClick={() => dispatch({ type: '**' })}>
